8ba29110be54667dd2bf2fa9bae5708ea982b681
Esperozzi <64204197+Esperozzi@users.noreply.github.com>
2020-04-25 12:45:45 +0200

    Niente di che

---
+++ Progetto Cicerone 2020/EPersonaggio.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class EPersonaggio
+{
+    private String $ID;
+    private String $Nome;
+    private String $Livello;
+    private String $Razza;
+    private String $Classe;
+    private String $Inventario;
+}
