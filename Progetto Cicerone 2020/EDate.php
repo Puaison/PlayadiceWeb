eb8f836b47dd389f91afdabe93a96cfa5376e797
elmamc <antonio.marottoli@gmail.com>
2020-04-27 21:16:12 +0200

    working on EDate.php

--- Progetto Cicerone 2020/EDate.php
+++ Progetto Cicerone 2020/EDate.php
@@ -5,24 +5,31 @@ class EDate
 {
     public array $listofDay=array("Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica");
     public array $listofMonth=array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre");
-    private string $currentDay; //decidere come caricare l'oggetto
-    private string $currentMonth;
-    private string $currentYear;
-    public array $thisDate;
+    private string $thisDay; //decidere come caricare l'oggetto
+    private string $thisMonth;
+    private string $thisYear;
+    public int $positionDate;
+    public DateTime $thisDate;
 
 
-    public function __construct($inputDate,$inputMonth,$inputYear){
 
-        $this->thisDate=array($this->listofDay[($inputDate)-1],$inputDate,$this->listofMonth[($inputMonth)-1],$inputYear);
 
-        $this->currentDay=date("w");
-        $this->currentMonth=date("n");
-        $this->currentYear=date("Y");
+    public function __construct(int $inputDate,int $inputMonth,int $inputYear, int $inputHour, int $inputMinutes){
+        $this->thisDate= date_create("$inputYear-$inputMonth-$inputDate-$inputHour-$inputMinutes");
+        $today=date_create();
+        $diff=date_diff($today,$this->thisDate);
+        $this->positionDate=$diff->format("%Y%M%d%H%i");
+        $this->thisMonth=$this->listofMonth[$inputMonth-1];
+        $this->thisDay=$this->listofDay[date_format($this->thisDate,"N")-1];
+        $this->thisYear=$inputYear;
+        echo $this->positionDate;
+
+
 
     }
     public function getDistance(){}
 
 }
+$date =new EDate(30,04,2020,20,30);
+var_dump($date);
 
-$today=new EDate(1,4,1993);
-var_dump($today);
