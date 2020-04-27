4f56d606613f2460498249f27bd9d9b403302d3b
elmamc <antonio.marottoli@gmail.com>
2020-04-27 13:23:56 +0200

    added variables event

--- Progetto Cicerone 2020/EEvent.php
+++ Progetto Cicerone 2020/EEvent.php
@@ -3,6 +3,15 @@
 
 class EEvent
 {
-  //Ci////
-    //Cose a caso//
+  public string $eventId;
+  public string $eventName;
+  public string $eventCategory;
+  public bool $flagBooking;
+  public Place $eventPlace;
+  public Date $endTime;
+  public Date $startTime;
+  public array $listofDate;
+  public array $listofBooked;
+
+
 }
