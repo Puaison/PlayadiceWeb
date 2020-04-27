b08526cf65b52ec59db689a24498a612c947f62d
elmamc <antonio.marottoli@gmail.com>
2020-04-27 16:04:09 +0200

    working on EDate.php

--- Progetto Cicerone 2020/EDate.php
+++ Progetto Cicerone 2020/EDate.php
@@ -3,15 +3,26 @@
 
 class EDate
 {
-    public array $listofDay;
-    public array $listofMonth;
-    public date $curranteDay; //decidere come caricare l'oggetto
-    public month $currentMonth;
-    public int $year;
+    public array $listofDay=array("Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica");
+    public array $listofMonth=array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre");
+    private string $currentDay; //decidere come caricare l'oggetto
+    private string $currentMonth;
+    private string $currentYear;
+    public array $thisDate;
 
-    public function __construct()
-    {
+
+    public function __construct($inputDate,$inputMonth,$inputYear){
+
+        $this->thisDate=array($this->listofDay[($inputDate)-1],$inputDate,$this->listofMonth[($inputMonth)-1],$inputYear);
+
+        $this->currentDay=date("w");
+        $this->currentMonth=date("n");
+        $this->currentYear=date("Y");
+
     }
     public function getDistance(){}
 
 }
+
+$today=new EDate(1,4,1993);
+var_dump($today);
