dddf6205a98e2b419d31ee68a0ac84eee31cd70b
elmamc <antonio.marottoli@gmail.com>
2020-04-27 15:05:54 +0200

    added EDate.php

---
+++ Progetto Cicerone 2020/EDate.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class EDate
+{
+    public array $listofDay;
+    public array $listofMonth;
+    public date $curranteDay; //decidere come caricare l'oggetto
+    public month $currentMonth;
+    public int $year;
+
+    public function __construct()
+    {
+    }
+    public function getDistance(){}
+
+}
--- Progetto Cicerone 2020/EPlace.php
+++ Progetto Cicerone 2020/EPlace.php
@@ -3,5 +3,15 @@
 
 class EPlace
 {
+    public string $placeAddress;
+    public string $placeCity;
+    public string $placeCap;
+
+    public function __construct($placeAddress,$placeCity,$placeCap)
+    {
+        $this->placeAddress=$placeAddress;
+        $this->placeCity=$placeCity;
+        $this->placeCap=$placeCap;
+    }
 
 }
