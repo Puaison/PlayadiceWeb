94f15b6b1be910c31636738fb4f05d78dee6b20c
elmamc <antonio.marottoli@gmail.com>
2020-04-28 10:17:30 +0200

    EDate v0.3

--- Progetto Cicerone 2020/EDate.php
+++ Progetto Cicerone 2020/EDate.php
@@ -1,23 +1,23 @@
 <?php
-
+declare(strict_types=1);
 
 class EDate
 {
     public array $listofDay=array("Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica");
     public array $listofMonth=array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre");
-    public string $thisDay;
+    public string $thisDayName;
     public string $thisDayNumb;
     public string $thisMonth;
     public string $thisYear;
-    public int $positionDate;
+    public string $positionDate;
     public DateTime $thisDate;
 
     public function __construct(string $inputDate,string $inputMonth,string $inputYear, string $inputHour, string $inputMinutes){
         $this->thisDate= date_create("$inputYear-$inputMonth-$inputDate"."$inputHour:$inputMinutes");
         $this->positionDate=EDate::getDistance($today=date_create());
         $this->thisMonth=$this->listofMonth[$inputMonth-1];
-        $this->thisDayNumb=date_format($this->thisDate, "d");
-        $this->thisDay=$this->listofDay[date_format($this->thisDate,"N")-1];
+        $this->thisDayNumb=date_format($this->thisDate, "j");
+        $this->thisDayName=$this->listofDay[date_format($this->thisDate,"N")-1];
         $this->thisYear=$inputYear;
     }
     public function getDistance($date){
@@ -27,7 +27,4 @@ class EDate
     }
 
 }
-$date =new EDate(28,04,2020,9,47);
-
-var_dump($date);
 
