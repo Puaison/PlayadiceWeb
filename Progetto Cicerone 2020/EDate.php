ff2aab6ad162966b6e030f6ff00ef0f6e2539b6d
elmamc <antonio.marottoli@gmail.com>
2020-05-01 19:37:53 +0200

    ebooking 0.1

--- Progetto Cicerone 2020/EDate.php
+++ Progetto Cicerone 2020/EDate.php
@@ -1,5 +1,7 @@
 <?php
 declare(strict_types=1);
+const LIST_DATE=array("Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica");
+CONST LIST_MONTH=array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre");
 
 class EDate
 {
@@ -11,13 +13,11 @@ class EDate
     public DateTime $thisDate;
 
     public function __construct(string $inputDate,string $inputMonth,string $inputYear, string $inputHour, string $inputMinutes){
-        array $listofDay=array("Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica");
-        array $listofMonth=array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre");
         $this->thisDate= date_create("$inputYear-$inputMonth-$inputDate"."$inputHour:$inputMinutes");
         $this->positionDate=EDate::getDistance($today=date_create());
-        $this->thisMonth=$listofMonth[$inputMonth-1];
+        $this->thisMonth=LIST_MONTH[$inputMonth-1];
         $this->thisDayNumb=date_format($this->thisDate, "j");
-        $this->thisDayName=$listofDay[date_format($this->thisDate,"N")-1];
+        $this->thisDayName=LIST_DATE[date_format($this->thisDate,"N")-1];
         $this->thisYear=$inputYear;
     }
     public function getDistance($date){
@@ -25,6 +25,17 @@ class EDate
         $diff=$diff->format("%Y%M%d%H%i%s");
         return $diff;
     }
+    public function setDay(int $day){$this->thisDayName=LIST_DATE[$day];}
+    public function setYear(string $year){$this->thisYear=$year;}
+    public function setMonth(string $month){$this->thisMonth=$month;}
+    public function setDayNum(int $day){$this->dateDayNumb=$day;}
+    public function setDate(DateTime $day){$this->thisDate=$day;}
+    public function getDayName(){return $this->thisDayName;}
+    public function getDayNumb(){return $this->thisDayNumb;}
+    public function getMonth(){return $this->thisMonth;}
+    public function getYear(){return $this->thisYear;}
+    public function getPosition(){return $this->positionDate;}
+    public function __toString(){return $print= $this->getDayNumb()."-".$this->getMonth()."-".$this->getYear();}
 
 }
 
