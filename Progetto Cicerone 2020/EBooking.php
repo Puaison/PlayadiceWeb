65f6e67cbab1d09433443da74e16264a48b544b5
elmamc <antonio.marottoli@gmail.com>
2020-05-01 18:45:32 +0200

    ebooking 0.1

--- Progetto Cicerone 2020/EBooking.php
+++ Progetto Cicerone 2020/EBooking.php
@@ -6,8 +6,18 @@ class EBooking
     public EDate $dateBooking;
     public string $userName;
     public string $userSurname;
-    public bool $bookState;
+    public bool $bookState=false;
 
+    public function __construct(Edate $date, string $name, string $surname)
+    {
+        $this->dateBooking=$date;
+        $this->userName=$name;
+        $this->userSurname=$surname;
+    }
+    public function setName($name){$this->userName=$name;}
+    public function setSurname($surname){$this->userSurname=$surname;}
+    public function validateState(){$this->bookState=true;}
+    public function setDate(EDate $date){$this->dateBooking=$date;}
 
 
 }
