f8e4face4c2e7fa6ce33e4078bfdb3effa6eb259
elmamc <antonio.marottoli@gmail.com>
2020-05-02 10:37:13 +0200

    booking 1.0 Date 1.0

--- Progetto Cicerone 2020/EPlace.php
+++ Progetto Cicerone 2020/EPlace.php
@@ -7,11 +7,21 @@ class EPlace
     public string $placeCity;
     public string $placeCap;
 
-    public function __construct(string $placeAddress, string $placeCity, string $placeCap)
+    public function __construct($placeAddress, string $placeCity, string $placeCap)
     {
         $this->placeAddress=$placeAddress;
         $this->placeCity=$placeCity;
         $this->placeCap=$placeCap;
+    }
+    public function setAddress($address){$this->placeAddress=$address;}
+    public function setCity($city){$this->placeCity=$city;}
+    public function setCap($cap){$this->placeCap=$cap;}
+    public function getAddress(){return $this->placeAddress;}
+    public function getCity(){return $this->placeCity;}
+    public function getCap(){return $this->placeCap;}
+    public function __toString()
+    {
+        return $print= "VIA: " . $this->getAddress() . " | CITTA': " . $this->getCity() . " | CAP: ". $this->getCap();
     }
 
 }
