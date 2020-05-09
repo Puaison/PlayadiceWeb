e210f13f61f06c5027326a05cd8381054ea67055
elmamc <antonio.marottoli@gmail.com>
2020-05-09 12:56:37 +0200

    event 0.1

--- Progetto Cicerone 2020/EEvento.php
+++ Progetto Cicerone 2020/EEvento.php
@@ -1,17 +1,18 @@
 <?php
 declare(strict_types=1);
 
-class EEvent
+class EEvento
 {
   public int $eventId;
   public string $eventName;
   public string $eventCategory;
   public bool $flagBooking;
-  public EPlace $eventPlace;
+  public ELuogo $eventPlace;
   public array $listofDate;
+  public array $listofBooked;
 
   public function __construct(int $lastEventId, string $name, string $category, bool $flag, EDate $startTime,
-                              EDate $endTime, EPlace $location, EDate ...$dates){
+                              EDate $endTime, ELuogo $location, EDate ...$dates){
       $this->eventPlace=$location;
       $this->eventId=$lastEventId+1;
       $this->eventName=$name;
@@ -35,7 +36,7 @@ class EEvent
       if ($this->flagBooking!=true)
           $this->listofBooked=array();
   }
-    public function setPlace(EPlace $place){$this->eventPlace=$place;}
+    public function setPlace(ELuogo $place){$this->eventPlace=$place;}
     public function setMinutes(string $minutes){$this->thisMinutes=$minutes;}
     public function setDate(EDate $day){array_splice($this->listofDate,-1 ,0,"$day");}
     public function setStartDate(EDate $day){array_splice($this->listofDate,0 ,1,"$day");}
--- Progetto Cicerone 2020/ELuogo.php
+++ Progetto Cicerone 2020/ELuogo.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-class EPlace
+class ELuogo
 {
     public string $placeAddress;
     public string $placeCity;
