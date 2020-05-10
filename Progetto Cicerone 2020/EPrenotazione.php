ac49b2a033c08d637e2ddc6bc91d302c4acdeb5d
elmamc <antonio.marottoli@gmail.com>
2020-05-10 17:41:28 +0200

    EPrenotazione.php 1.0 con php docs

--- Progetto Cicerone 2020/EPrenotazione.php
+++ Progetto Cicerone 2020/EPrenotazione.php
@@ -1,32 +1,62 @@
 <?php
 
 
+/**
+ * Class EPrenotazione Rappresenta la prenotazione che un utante può fare ad un evento
+ */
 class EPrenotazione extends EObject
 {
-    public EData $giornoPrenotazione;
-    public EUtente $utente;
+    /**
+     * @var EData Giorno in cui è stata effettuata la prenotazione
+     */
+    private $giornoPrenotazione;
+    /**
+     * @var EUtente Utente che ha effettuato la prenotazione
+     */
+    private EUtente $utente;
 
-    public function __construct()
+    /**
+     * EPrenotazione constructor.Inizializza un oggetto Prenotazione vuoro
+     */
+    function __construct()
     {
+        parent::__construct();
         $this->giornoPrenotazione= new EData();
         $this->utente= new EUtente();
-
     }
 
-    public function getId(): int
-    {
-        return $this -> id;
-    }
 
-    public function setId(int $id): void
-    {
-        $this -> id = $id;
-    }
-    public function setUtente(EUtente $utente){$this->utente=$utente;}
-    public function setDate(EData $date){$this->giornoPrenotazione=$date;}
-    public function getUtente(){return $this->utente;}
-    public function getDate(){return $this->giornoPrenotazione;}
-    public function __toString()
+    /**
+     *
+     *                                                   METODI SET
+     * Metodo per impostare l'utente
+     * @param EUtente $utente l'utente
+     */
+    function setUtente(EUtente $utente){$this->utente=$utente;}
+
+    /**
+     * Metodo per impostare la data di creazione della prenotazione
+     * @param EData $date la data della prenotazione
+     */
+    function setDate(EData $date){$this->giornoPrenotazione=$date;}
+
+    /**
+     * Metodo che restituisce l'utente che ha fatto la prenotazione
+     * @return EUtente
+     */
+    function getUtente(){return $this->utente;}
+
+    /**
+     * Metodo che restituisce la data di creazione della prenotazione
+     * @return EData
+     */
+    function getDate(){return $this->giornoPrenotazione;}
+
+    /**
+     * Metodo che restituisce le informazioni della prenotazione in forma di stringa
+     * @return string
+     */
+    function __toString()
     {
         return $print= "NOME: " . $this->utente->getName() . " | COGNOME: " . $this->utente->getSurname() . " | DATA DI PRENOTAZIONE: ". $this->getDate();
     }
