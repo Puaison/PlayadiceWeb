d58cfcdc6eda5a55421a9fc2d87320c3ba63992d
elmamc <antonio.marottoli@gmail.com>
2020-05-21 18:24:10 +0200

    updated autoload.php

--- Progetto Cicerone 2020/autoload.php
+++ Progetto Cicerone 2020/autoload.php
@@ -1,11 +1,25 @@
 <?php
 function my_autoload($classe)
-    {$entity=__DIR__."/"."Entity/".$classe.".php";
-    if(file_exists($entity))
-        echo ($entity);
-    echo"\n";
+{
+    $entity = __DIR__ . "/" . "Entity/" . $classe . ".php";
+    $foundation = __DIR__ . "/" . "Foundation/" . $classe . ".php";
+    if (file_exists($entity)) {
+        echo($entity);
+        echo "\n";
         include $entity;
-       }
+    }
+    elseif (file_exists($foundation)) {
+        echo($foundation);
+        echo "\n";
+        include $foundation;
+    }
+    else {
+        return false;
+    }
+    return true;
+}
+
+
 
 
 spl_autoload_register("my_autoload");
