6d3fcf92dc1b096f29bf2c119c40fa2d249dab68
Esperozzi <64204197+Esperozzi@users.noreply.github.com>
2020-07-12 15:44:45 +0200

    Tutte i casi d'uso del profilo implementate

--- Controller/CAdmin.php
+++ Controller/CAdmin.php
@@ -33,15 +33,19 @@ class CAdmin
     {
         $vAdmin=new VAdmin();
         $utente=CSession::getUserFromSession();
-        $utente=FPersistantManager::getInstance()->search('utente','UserName',$utente->getUsername())[0];
+
         if(get_class($utente)==EAdmin::class) // se l'utente è admin
         {
             if($Username)
             {
                 $Banned=FPersistantManager::getInstance()->search('utente','UserName',$Username)[0];
                 FPersistantManager::getInstance()->remove($Banned);
+                CAdmin::openAdminPanel();
             }
-            CAdmin::openAdminPanel();
+            else
+            {
+                $vAdmin->showErrorPage($utente,"Errore, Username non esistente");
+            }
         }
         else
         {
@@ -49,12 +53,73 @@ class CAdmin
         }
     }
 
-    static function removeUtente()
+    static function RendiAdmin($Username = "")
     {
-        $user=CSession::getUserFromSession();
-        CSession::destroySession();
-        FPersistantManager::getInstance()->remove($user);
-        header('Location: /playadice/index');
+        $vAdmin=new VAdmin();
+        $utente=CSession::getUserFromSession();
+
+        if(get_class($utente)==EAdmin::class) // se l'utente è admin
+        {
+            if($Username) // se ci sta il parametro
+            {
+                $NewAdmin = new EAdmin();
+                $Promoted = FPersistantManager::getInstance()->search('utente','UserName',$Username)[0];
+                $NewAdmin ->setNome($Promoted->getNome());
+                $NewAdmin ->setCognome($Promoted->getCognome());
+                $NewAdmin ->setUsername($Promoted->getUsername());
+                $NewAdmin ->setPassword($Promoted->getPassword());
+                $NewAdmin ->setEmail($Promoted->getmail());
+
+                FPersistantManager::getInstance()->update($NewAdmin);
+                CAdmin::openAdminPanel();
+            }
+            else
+            {
+                $vAdmin->showErrorPage($utente,"Errore, Username non esistente");
+            }
+        }
+        else
+        {
+            $vAdmin->showErrorPage($utente,"Devi essere Admin per accedere a questa pagina");
+        }
     }
+
+    static function RevocaAdmin($Username = "")
+    {
+        $vAdmin=new VAdmin();
+        $utente=CSession::getUserFromSession();
+
+        if(get_class($utente)==EAdmin::class) // se l'utente è admin
+        {
+            if($Username != $utente->getUsername()) // se sto tentando di cancellarmi in quanto admin
+            {
+                if ($Username) // se ci sta il parametro
+                {
+                    $NewUtente = new EUtente();
+                    $Revoked = FPersistantManager::getInstance()->search('utente', 'UserName', $Username)[0];
+                    $NewUtente->setNome($Revoked->getNome());
+                    $NewUtente->setCognome($Revoked->getCognome());
+                    $NewUtente->setUsername($Revoked->getUsername());
+                    $NewUtente->setPassword($Revoked->getPassword());
+                    $NewUtente->setEmail($Revoked->getmail());
+
+                    FPersistantManager::getInstance()->update($NewUtente);
+                    CAdmin::openAdminPanel();
+                } else {
+                    $vAdmin->showErrorPage($utente, "Errore, Username non esistente");
+                }
+            }
+            else
+            {
+                $vAdmin->showErrorPage($utente,"Non puoi rimuovere te stesso");
+            }
+        }
+        else
+        {
+            $vAdmin->showErrorPage($utente,"Devi essere Admin per accedere a questa pagina");
+        }
+    }
+
+
 
 }
